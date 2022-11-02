b1a42697ccf0f884b3d20167ed82cc7fe0d810ea
SHongKyo <hongkyo0411@naver.com>
2022-11-02 16:02:19 +0900

    1102

---
+++ shells/report2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+man=1
+woman=2
+if [ $man -lt $woman ]
+then
+	echo woman1
+fi
--- shells/useradd.sh
+++ shells/useradd.sh
@@ -7,7 +7,7 @@ cnt=$2
 while [ $i -le $cnt ]; do
 	let uid+=1
 	useradd -u $uid -g users -s /bin/bash -m user$i
-	user$i
-	let i +=1
+	passwd -d user$i
+	let i+=1
 done
 echo Complete!!
