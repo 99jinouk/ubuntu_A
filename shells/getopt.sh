c8b091ac52bab3daacafdc58bc9a1b3f14886a44
SHongKyo <hongkyo0411@naver.com>
2022-11-02 16:29:56 +0900

    1102

--- shells/getopt.sh
+++ shells/getopt.sh
@@ -3,14 +3,14 @@
 set -- $(getopt -q ab:cd "$@")
 while [ -n $1 ]; do
 	case $1 in
-		-a) echo "Found the -a option" ;;
+		-a) echo "Found the -a option1" ;;
 		-b) param=$2
-		    echo "Found the -b option with parameter value $param"
+		    echo "Found the -b option2 with parameter value $param"
 				shift ;;
-	  -c) echo "Found the -c option";;
+	  -c) echo "Found the -c option3";;
 		--) shift
 		    break ;;
-	   *) echo "$1 is not an option" ;;
+	   *) echo "$1 is not an option4" ;;
 	 esac
 	 shift
  done
--- shells/report2.sh
+++ shells/report2.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-man=1
-woman=2
-if [ $man -lt $woman ]
-then
-	echo woman1
-fi
+set -- $(getopt -q ab:cd "$@")
+while [ -n $1 ]; do
+	case $1 in
+		-a) echo "Found the -a option1" ;;
+		-b) param=$2
+		    echo "Found the -b option2 with parameter value $param"
+				shift ;;
+	  -c) echo "Found the -c option3";;
+		--) shift
+		    break ;;
+	   *) echo "$1 is not an option4" ;;
+	 esac
+	 shift
+ done
+ count=1
+ for param in $@ ; do
+	 echo "parameter #$count : $param"
+	 count=$[ $count + 1 ]
+ done
