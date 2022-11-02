825bee1b9832b8ca4efaf6a47b5504df7cd992c9
SHongKyo <hongkyo0411@naver.com>
2022-11-02 14:46:40 +0900

    123

--- shells/useradd.sh
+++ shells/useradd.sh
@@ -6,7 +6,8 @@ cnt=$2
 
 while [ $i -le $cnt ]; do
 	let uid+=1
-	useradd -u $uid -g users -s /bin/bash
-	-m user$if [[ passwd -d user$if [[ let i +=1
+	useradd -u $uid -g users -s /bin/bash -m user$i
+	user$i
+	let i +=1
 done
 echo Complete!!
