b7dffd6c55aa67c966c984ad7ae3cf85e133f621
SHongKyo <hongkyo0411@naver.com>
2022-11-09 13:21:17 +0900

    123

--- shells/report2.sh
+++ shells/report2.sh
@@ -3,13 +3,13 @@
 if [ $# -eq 3 ];then
 while [ -n $2 ];do
   case $2 in
-    +) echo "41836041 $1 + $3 = `expr $1 + $3`"
+    +) echo "$1 + $3 = `expr $1 + $3`"
     break;;
-    -) echo "41836041 $1 - $3 = `expr $1 - $3`"
+    -) echo "$1 - $3 = `expr $1 - $3`"
     break;;
-    X) echo "41836041 $1 X $3 = `expr $1 \* $3`"
+    X) echo "$1 X $3 = `expr $1 \* $3`"
     break;;
-    /) echo "41836041 $1 / $3 = `expr $1 / $3`"
+    /) echo "$1 / $3 = `expr $1 / $3`"
     break;;
     *) echo "second prameter is only '+ = X /'"
     break;;
