5d77ebc2e92a544c3e1ae196c19e2206c1acac0e
SHongKyo <hongkyo0411@naver.com>
2022-11-09 13:22:15 +0900

    123

--- shells/report22.sh
+++ shells/report22.sh
@@ -4,10 +4,10 @@ if [ $# -ne 3 ]; then
   echo "this program require theree paramters..."
 elif [ $# -eq 3 ]; then
   case $2 in
-    +) echo "41836041 $1 + $3 = `expr $1 + $3`"
-    -) echo "41836041 $1 - $3 = `expr $1 - $3`"
-    X) echo "41836041 $1 X $3 = `expr $1 \* $3`"
-    /) echo "41836041 $1 / $3 = `expr $1 / $3`"
+    +) echo "$1 + $3 = `expr $1 + $3`"
+    -) echo "$1 - $3 = `expr $1 - $3`"
+    X) echo "$1 X $3 = `expr $1 \* $3`"
+    /) echo "$1 / $3 = `expr $1 / $3`"
     *) echo "second prameter is only '+ = X /'"
   esac
 fi
